a45f9bc8cf475ccf51918bedda9e4fe7a6e8cd14
Yami939 <150692981+Yami939@users.noreply.github.com>
2024-10-04 12:15:37 -0300

    trabalho reposição de aula

---
+++ main.py
@@ -0,0 +1,13 @@
+print ('INFORMAÇÕES USUARIO')
+
+print ('Olá Usuario, poderia informar seu nome por favor?')
+
+nome_do_usuario = input('Digite o seu nome aqui: ')  
+
+print ('Obrigado,', nome_do_usuario ,',agora poderia me informar sua idade? ')
+
+idade_do_usuario = (input('Digite a sua idade'))
+
+print (idade_do_usuario)
+
+print ('Olá ', nome_do_usuario, 'vejo que voce tem', idade_do_usuario ,'anos, seja bem vindo')
